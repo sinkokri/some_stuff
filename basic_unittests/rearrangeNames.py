271afec86bb0a50743c81ecae5fe5fa6f179589c
Kristina <sinkovskaya.kristina@gmail.com>
2020-09-09 16:51:33 +0200

    raised error

--- basic_unittests/rearrangeNames.py
+++ basic_unittests/rearrangeNames.py
@@ -2,7 +2,11 @@
 import re 
 
 def rearrange(name):
+    if not isinstance(name, str):
+        raise TypeError(f'it has to be a name in formar "name, surname"')
     result=re.search(r"^([\w .]*), ([\w .]*)$", name)
     if result is None:
         return name
     return "{} {}".format(result[2], result[1])
+
+print(rearrange("Maria, Petrova"))
