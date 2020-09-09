1071063d7dd702f2b84b8afa0bc65a756c2e915b
Kristina <sinkovskaya.kristina@gmail.com>
2020-09-09 13:58:25 +0200

    added assertnotequal

--- basic_unittests/test_rearrange.py
+++ basic_unittests/test_rearrange.py
@@ -9,20 +9,25 @@ class TestRearrange(unittest.TestCase):
         testcase1="Kristina, Sinkovskaya"
         expected1="Sinkovskaya Kristina"
         self.assertEqual(rearrange(testcase1), expected1)
+        self.assertNotEqual(rearrange(testcase1), testcase1)
         testcase2="John, Wick"
         expected2="Wick John"
         self.assertEqual(rearrange(testcase2),expected2)
+        self.assertNotEqual(rearrange(testcase2), testcase2)
     def test_empty(self):
         testcase=""
         expected=""
         self.assertEqual(rearrange(testcase), expected)
+        self.assertNotEqual(rearrange(testcase), "Ivanka Trump")
     def test_double_name(self):
         testcase="Novakova, Marie M."
         expected="Marie M. Novakova"
         self.assertEqual(rearrange(testcase), expected)
+        self.assertNotEqual(rearrange(testcase), testcase)
     def test_one_name(self):
         testcase="Voldemort"
         expected="Voldemort"
         self.assertEqual(rearrange(testcase), expected)
+        self.assertNotEqual(rearrange(testcase), sorted(testcase))    
 unittest.main()
 
